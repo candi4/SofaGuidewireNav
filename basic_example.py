c5323acdfea0810e34d9dc45d58737b19d8d4902
candi4 <hojun327@naver.com>
2024-05-01 00:30:18 +0900

    Revise example to avoid saving error

--- basic_example.py
+++ basic_example.py
@@ -7,6 +7,6 @@ for i in range(10000):
     sofa.action(translation=1,rotation=0.1)
     sofa.step()
     image = sofa.GetImage()
-    SaveImage(image, f'image/screen{i%10}.jpg')
+    SaveImage(image, f'image/screen{i%50}.jpg')
 
 
